18aceefc76e97b0d55f1f55c6bd905a4e7f36856
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-05-07 16:14:01 +0100

    set up components, import to App and render

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="Navbar--container">
+            <h1>Navbar component</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
